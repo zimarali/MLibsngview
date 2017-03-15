8287df807bdc27e7beb0857591491a200318e1f5
Ramiz Ali <zimarali@gmail.com>
2017-03-15 18:57:05 -0400

    edit on noun

--- scripts/factory.js
+++ scripts/factory.js
@@ -9,13 +9,13 @@ app.factory('madlibsFactory', function() {
     mlInfo: mlInfo
   };
 
-  function mlPass(name, verb1, adj1, place, noun2, verb2, adj2) {
+  function mlPass(name, verb1, adj1, place, noun, verb2, adj2) {
 
     mlInput.name = name;
     mlInput.verb1 = verb1;
     mlInput.adj1 = adj1;
     mlInput.place = place;
-    mlInput.noun2 = noun2;
+    mlInput.noun = noun;
     mlInput.verb2 = verb2;
     mlInput.adj2 = adj2;
 
--- scripts/userinput.js
+++ scripts/userinput.js
@@ -2,11 +2,13 @@ var app = angular.module('madlibsApp');
 
 app.controller('inputCtrl', function($scope, $location, madlibsFactory){
 
-  $scope.sendInput = function(name, verb1, adj1, place, noun2, verb2, adj2){
+  $scope.sendInput = function(name, verb1, adj1, place, noun, verb2, adj2){
 
-     madlibsFactory.mlPass(name, verb1, adj1, place, noun2, verb2, adj2);
+     madlibsFactory.mlPass(name, verb1, adj1, place, noun, verb2, adj2);
 
-     console.log(name, verb1, adj1, place, noun2, verb2, adj2);
+     //$location.path('/inputdisplay');
+
+     //console.log(name, verb1, adj1, place, noun, verb2, adj2);
 
   }
 
