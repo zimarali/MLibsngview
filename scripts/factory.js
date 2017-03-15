b8405c33999f6e16e9b52a8d4a667264b70fc548
Ramiz Ali <zimarali@gmail.com>
2017-03-14 21:22:09 -0400

    display page with stuff

--- scripts/factory.js
+++ scripts/factory.js
@@ -9,11 +9,12 @@ app.factory('madlibsFactory', function() {
     mlInfo: mlInfo
   };
 
-  function mlPass(name, verb1, adj1, noun2, verb2, adj2) {
+  function mlPass(name, verb1, adj1, place, noun2, verb2, adj2) {
 
     mlInput.name = name;
     mlInput.verb1 = verb1;
     mlInput.adj1 = adj1;
+    mlInput.place = place;
     mlInput.noun2 = noun2;
     mlInput.verb2 = verb2;
     mlInput.adj2 = adj2;
--- scripts/userinput.js
+++ scripts/userinput.js
@@ -2,9 +2,11 @@ var app = angular.module('madlibsApp');
 
 app.controller('inputCtrl', function($scope, $location, madlibsFactory){
 
-  $scope.sendInput = function(noun1, verb1, adj1, noun2, verb2, adj2){
+  $scope.sendInput = function(name, verb1, adj1, place, noun2, verb2, adj2){
 
-     madlibsFactory.mlPass(noun1, verb1, adj1, noun2, verb2, adj2)
+     madlibsFactory.mlPass(name, verb1, adj1, place, noun2, verb2, adj2);
+
+     console.log(name, verb1, adj1, place, noun2, verb2, adj2);
 
   }
 
