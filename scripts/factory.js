f96e60f27bc21062af874014c25208f065c372a0
Ramiz Ali <zimarali@gmail.com>
2017-03-15 20:52:37 -0400

    added OR (words)

--- scripts/factory.js
+++ scripts/factory.js
@@ -2,20 +2,20 @@ var app = angular.module('madlibsApp');
 
 app.factory('madlibsFactory', function() {
 
-  var mlInput = {};
+  var mlInput = [];
 
   return {
     mlPass: mlPass,
     mlInfo: mlInfo
   };
 
-  function mlPass(name, verb1, adj1, place, noun, verb2, adj2) {
+  function mlPass(name, verb1, adj1, place, noun1, verb2, adj2) {
 
     mlInput.name = name;
     mlInput.verb1 = verb1;
     mlInput.adj1 = adj1;
     mlInput.place = place;
-    mlInput.noun = noun;
+    mlInput.noun1 = noun1;
     mlInput.verb2 = verb2;
     mlInput.adj2 = adj2;
 
